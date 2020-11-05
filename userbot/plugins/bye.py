c18dc22969b2c9b8f828b2b690a1cadae54deaea
thebadi <70918413+thebadi@users.noreply.github.com>
2020-11-05 11:38:43 +0100

    Update bye.py
    bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -18,9 +18,9 @@ from userbot.system import dev_cmd
 @bot.on(dev_cmd("bye", outgoing=True))
 async def bye(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit('**Esco dalla chat!**')
+        await e.edit('**Esco Da Sta Chat Di Merda Bye!🤡**')
         time.sleep(3)
         if '-' in str(e.chat_id):
             await bot(LeaveChannelRequest(e.chat_id))
         else:
-            await e.edit('**Non puoi abbandonare la chat**')
+            await e.edit('**@Lqmpo Ha abbandonato la chat...**')
